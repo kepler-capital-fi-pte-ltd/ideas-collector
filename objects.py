2180870920b630c42dbd8b8091945e1bbbdee77f
Robbie <robbie@keplerfi.com>
2019-09-11 15:36:00 -0400

    objects

---
+++ objects.py
@@ -0,0 +1,21 @@
+import datetime
+
+class Idea(object):
+    def __init__(iden, barrid, company, author, market, position, thesis, metadata):
+        self.iden = iden
+        self.created = datetime.now()
+        self.barrid = barrid
+        self.company = company
+        self.author = author
+        self.market = market
+        self.position = position
+        self.thesis = thesis
+        self.metadata = metadata
+
+class User(object):
+    def __init__(iden, username, password, born, status):
+        self.iden = iden
+        self.username = username
+        self.password = password
+        self.born = datetime.now()
+        self.status = status
