b456ad40411214caf09fe1af0301381e2d4dd86e
Robbie <robbie@keplerfi.com>
2019-09-11 17:02:25 -0400

    update db.py with objects

--- db.py
+++ db.py
@@ -8,14 +8,23 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = db
 db = SQLAlchemy(app)
 
+class Idea(db.Model):
+# flask-sqlalchemy db object to store the ideas in the db
+    iden = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.Date, unique=True, nullable=False)
+    barrid = db.Column(db.String, unique=True, nullable=False)
+    company = db.Column(db.String, unique=True, nullable=False)
+    author = db.Column(db.String, unique=True, nullable=False)
+    market = db.Column(db.String, unique=True, nullable=False)
+    position = db.Column(db.String, unique=True, nullable=False)
+    thesis = db.Column(db.String, unique=True, nullable=False)
+    metadata = db.Column(db.String, unique=True, nullable=False)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+# flask-sqlalchemy db object to store the users
+    iden = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
-
-
-db.session.add(User(username="Flask", email="example@example.com"))
-db.session.commit()
-
-users = User.query.all()
+    born = db.Column(db.Date, unique=True, nullable=False)
+    status = db.Column(db.String, unique=True, nullable=False)
