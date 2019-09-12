9dcc3a1f4ce301ec8e396c4687c87f01381b8260
Robbie <robbie@keplerfi.com>
2019-09-12 12:39:32 -0400

    add api

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 user = "admin"
 password = "Summer2019"
@@ -8,19 +9,25 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = db
 db = SQLAlchemy(app)
 
+"""
+This file creates all of the classes for the SQL-Alchemy ORM.
+For each class, SQL-Alchemy will create a DB table to store data.
+:reuters_id: is our unique identifier (not ticker)
+:iden: is our internal identifier
+"""
 
 class Idea(db.Model):
     # flask-sqlalchemy db object to store the ideas in the db
+    #TODO add linkage between the author and the author
     iden = db.Column(db.Integer, primary_key=True)
-    created = db.Column(db.Date, nullable=False)
-    barrid = db.Column(db.String, nullable=False)
+    created = db.Column(db.DateTime, nullable=False)
+    reuters_id = db.Column(db.String, nullable=False)
     company = db.Column(db.String, nullable=False)
     author = db.Column(db.String, nullable=False)
     market = db.Column(db.String, nullable=False)
     position = db.Column(db.String, nullable=False)
     thesis = db.Column(db.String, nullable=False)
     metadata = db.Column(db.String, nullable=False)
-
 
 class User(db.Model):
     # flask-sqlalchemy db object to store the users
@@ -31,12 +38,14 @@ class User(db.Model):
     born = db.Column(db.Date, nullable=False)
     status = db.Column(db.String, nullable=False)
 
-
 class Performance(db.Model):
-    # flask-sqlalchemy db object to store the performance
+    # flask-sqlalchemy db object to store the performance data
     iden = db.Column(db.Integer, primary_key=True)
-    ticker = db.Column(db.String, nullable=False)
+    reuters_id = db.Column(db.String, nullable=False)
     L1M = db.Column(db.Numeric)
     L1Y = db.Column(db.Numeric)
     YTD = db.Column(db.Numeric)
     since_inception = db.Column(db.Numeric)
+
+#TODO add ds2 pricing table schema
+#TODO add a table with company name, ticker, RIC (i.e mapping table) for search
